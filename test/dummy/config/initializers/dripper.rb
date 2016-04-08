43d1ac89c4494584f4f98fc81a794ef93e11aea1
Douglas Tarr <douglas.tarr@gmail.com>
2016-04-08 11:01:28 -0700

    remove non-existent models

--- test/dummy/config/initializers/dripper.rb
+++
@@ -1,6 +0,0 @@
-Dripper.config model: :users do
-  dripper mailer: :user_mailer do
-    dripper action: :welcome
-    dripper action: :newsletter, wait: 1.minutes, scope: -> {has_username}
-  end
-end
